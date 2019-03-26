00c53d75feae3c99b1cfb40afdcccb66c33c6bb3
refrain <nao_h13@hotmail.com>
2019-03-27 00:27:42 +0900

    P25 The Second test の実装により ガーターゲームのスコア計算ループ部分を再利用出来るようにメソッド化（テストOKになることも確認）

--- src/test/java/kata/bowling/BowlingGameKataTest.java
+++ src/test/java/kata/bowling/BowlingGameKataTest.java
@@ -19,13 +19,19 @@ public class BowlingGameKataTest extends TestCase
 		int pins = 0;
 		
 		// 全てのゲームのロールでスコア0を設定
+		rollMany( n, pins );
+		
+		// スコアが0であることを確認
+		assertEquals( 0, g.score() );
+	}
+	
+	// 指定されたピン数ですべてロールのスコアを設定
+	private void rollMany( int n, int pins )
+	{
 		for( int i = 0; i < n; i++ )
 		{
 			g.roll( pins );
 		}
-		
-		// スコアが0であることを確認
-		assertEquals( 0, g.score() );
 	}
 	
 	public void testAllOnes() throws Exception
