34e81d51c57df668606ae50e0691526f8127b613
refrain <nao_h13@hotmail.com>
2019-03-27 00:09:12 +0900

    P15 - P18 の The first test の実装をし「expected:<0> but was:<-1>」でエラーになることを確認

--- src/main/java/kata/bowling/Game.java
+++ src/main/java/kata/bowling/Game.java
@@ -3,5 +3,16 @@ package kata.bowling;
 // ボーリングのゲーム管理
 public class Game
 {
+	// ゲームロール
+	public void roll(int pins)
+	{
+		
+	}
+
+	// スコアの取得
+	public int score()
+	{
+		return -1;
+	}
 
 }
--- src/test/java/kata/bowling/BowlingGameKataTest.java
+++ src/test/java/kata/bowling/BowlingGameKataTest.java
@@ -8,5 +8,14 @@ public class BowlingGameKataTest extends TestCase
 	public void testGutterGame() throws Exception
 	{
 		Game g = new Game();
+		
+		// 全てのゲームのロールでスコア0を設定
+		for( int i = 0; i < 20; i++ )
+		{
+			g.roll( 0 );
+		}
+		
+		// スコアが0であることを確認
+		assertEquals( 0, g.score() );
 	}
 }
