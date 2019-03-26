17b28bb2771e15d0a34d0e5e07f931bd98f54bc5
refrain <nao_h13@hotmail.com>
2019-03-27 00:34:56 +0900

    P28 The Second test の実装により共通メソッドを上部に移動

--- src/test/java/kata/bowling/BowlingGameKataTest.java
+++ src/test/java/kata/bowling/BowlingGameKataTest.java
@@ -7,9 +7,19 @@ public class BowlingGameKataTest extends TestCase
 	// ゲーム管理
 	private Game g;
 	
+	// ゲームのインスタンスの生成
 	protected void setUp() throws Exception
 	{
 		this.g = new Game();
+	}
+
+	// 指定されたピン数で指定ロール数のスコアを設定
+	private void rollMany( int n, int pins )
+	{
+		for( int i = 0; i < n; i++ )
+		{
+			g.roll( pins );
+		}
 	}
 	
 	// ガーターゲームのテスト
@@ -20,15 +30,6 @@ public class BowlingGameKataTest extends TestCase
 		
 		// スコアが0であることを確認
 		assertEquals( 0, g.score() );
-	}
-	
-	// 指定されたピン数で指定ロール数のスコアを設定
-	private void rollMany( int n, int pins )
-	{
-		for( int i = 0; i < n; i++ )
-		{
-			g.roll( pins );
-		}
 	}
 	
 	// すべてのゲームロールで１だった場合のテスト
